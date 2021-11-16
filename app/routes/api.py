c45abd85e1653b5596141893252312d28c91356f
Eric Duwe <ericduwe@gmail.com>
2021-11-16 14:45:56 -0600

    added login_required decorater to routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ import sys
 from flask import Blueprint, request, jsonify, session
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
+from app.utils.auth import login_required
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -33,6 +34,7 @@ def signup():
 
 #create post route
 @bp.route('/posts', methods=['POST'])
+@login_required
 def create():
   data = request.get_json()
   db = get_db()
@@ -57,6 +59,7 @@ def create():
 
 #update post route
 @bp.route('/posts/<id>', methods=['PUT'])
+@login_required
 def update(id):
   data = request.get_json()
   db = get_db()
@@ -74,6 +77,7 @@ def update(id):
 
 #delete post route
 @bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
 def delete(id):
   db = get_db()
 
@@ -91,6 +95,7 @@ def delete(id):
 
 #comment route
 @bp.route('/comments', methods=['POST'])
+@login_required
 def comment():
   data = request.get_json()
   db = get_db()
@@ -115,6 +120,7 @@ def comment():
 
 #upvote route
 @bp.route('/posts/upvote', methods=['PUT'])
+@login_required
 def upvote():
   data = request.get_json()
   db = get_db()
